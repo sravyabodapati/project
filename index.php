fd0672c1e8b56feafe68c3d55eb9bf19a6257fec
sravyabodapati <sravyabodapati@gmail.com>
2019-11-19 16:26:13 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <link href="./Bootstrap_4.3.1/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Sravya Bodapati, Period 1]</p>'; ?> 
+ 
+       <a href="index.html">Home</a>
+       <a href="Bodapati_AboutMe.html">About Me</a>
+       <a href="Bodapati_Projects.html">Projects</a>
+       <a href="Bodapati_Resume.html">Resume</a>
+       <a href="Bodapati_ContactMe.html">Contact Me</a>
+
+  </body>
+</html>
